ed1dfe96c2bd1cc44440d4364d29c2dd44f283e0
Uwabor, Collins <legendarycolins@yahoo.com>
2023-12-23 04:49:57 +0100

    updated

---
+++ Compute Engine: Qwik Start - Windows/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ZONE=us-central1-c
+
+
+curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Compute%20Engine%20Qwik%20Start%20Windows/quicklabgsp093.sh
+
+sudo chmod +x quicklabgsp093.sh
+
+./quicklabgsp093.sh
