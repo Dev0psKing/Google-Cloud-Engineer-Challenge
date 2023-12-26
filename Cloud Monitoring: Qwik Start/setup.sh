454dd9e309b22495e598d1e6de9f8dea00249e42
Uwabor, Collins <legendarycolins@yahoo.com>
2023-12-26 16:01:26 +0100

    updated

---
+++ Cloud Monitoring: Qwik Start/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export ZONE=
+
+# Download the solution script
+curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Cloud%20Monitoring%20Qwik%20Start/quicklabgsp089.sh
+
+# Make the script executable
+sudo chmod +x quicklabgsp089.sh
+
+# Run the script
+./quicklabgsp089.sh
